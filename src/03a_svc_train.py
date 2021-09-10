141e962ec92d73cdfc9dddd94ec4ea12dae49df6
ultraxav <ultraxav@msn.com>
2021-09-09 21:08:45 -0300

    svc_train

--- src/03a_svc_train.py
+++ src/03a_svc_train.py
@@ -79,7 +79,8 @@ len(nombres_features) * 0.10
 # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFECV.html
 
 # %%
-# Se crea un modelo sin entonar, el razonamiento es el siguiente, si una variable es altamente predictora, figurará como predictora aún cuando el modelo es sencillo
+# Se crea un modelo sin entonar, el razonamiento es el siguiente, si una variable es altamente
+# predictora, figurará como predictora aún cuando el modelo es sencillo
 learner = SVC(
     kernel='linear', probability=True, class_weight='balanced', random_state=seed
 )
@@ -96,20 +97,19 @@ rfecv_svc = RFECV(
     cv=StratifiedKFold(3, random_state=seed),
     scoring='neg_log_loss',
     min_features_to_select=min_features_to_select,
+    n_jobs=-1,
 )
-
-rfecv_svc.fit(Xtrain, ytrain)
-
-print(f'Optimal number of features: {rfecv_svc.n_features_}')
 
 # %%
 # %%time
-rfecv_svc.fit(X, y)
+rfecv_svc.fit(Xtrain, ytrain)
 
 # %% [markdown]
 # inspección visual y seleccion de cantidad de variables importantes
 
 # %%
+print(f'Optimal number of features: {rfecv_svc.n_features_}')
+
 plt.figure(figsize=(10, 8))
 plt.xlabel('Number of features selected')
 plt.ylabel('Cross validation score (Negative Log Loss)')
@@ -117,4 +117,87 @@ plt.plot(
     range(min_features_to_select, len(rfecv_svc.grid_scores_) + min_features_to_select),
     rfecv_svc.grid_scores_,
 )
+
 plt.show()
+
+# %% [markdown]
+# # Sequential Feature Selector
+#
+# Ya sabiendo la cantidad de variables utilizaremos este algoritmo para obtener los nombres de las variables seleccionadas
+#
+# > This Sequential Feature Selector adds (forward selection) or removes (backward selection) features to form a feature subset in a greedy fashion. At each stage, this estimator chooses the best feature to add or remove based on the cross-validation score of an estimator. **Scikit-Learn**
+#
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SequentialFeatureSelector.html
+
+# %%
+learner = SVC(
+    kernel='linear', probability=True, class_weight='balanced', random_state=seed
+)
+
+# Buscará las 30000 variables más importantes
+n_features_to_select = 30000
+
+sfscv_svc = SequentialFeatureSelector(
+    estimator=learner,
+    n_features_to_select=n_features_to_select,
+    direction='backward',
+    scoring='neg_log_loss',
+    cv=StratifiedKFold(3, random_state=seed),
+    n_jobs=-1,
+)
+
+# %%
+# %%time
+sfscv_svc.fit(Xtrain, ytrain)
+
+# %%
+Xtrain_sel = Xtrain[sfscv_svc.get_support()]
+Xnames_sel = nombres_features[sfscv_svc.get_support()]
+
+# %% [markdown]
+# # Optimización Bayesiana
+
+# %%
+learner = SVC(
+    kernel='linear', probability=True, class_weight='balanced', random_state=seed
+)
+
+# Hiperparámetro a optimizar
+param = {'C': optuna.distributions.LogUniformDistribution(1e-10, 1e10)}
+
+# inicialización del experimento
+svc_search = optuna.integration.OptunaSearchCV(
+    learner,
+    param,
+    cv=StratifiedKFold(3, random_state=seed),
+    n_jobs=-1,
+    n_trials=100,
+    random_state=seed,
+    study=optuna.create_study(
+        study_name='news_svc', direction='maximaze', storage='sqlite:///news_svc.db'
+    ),
+)
+
+# %%
+svc_search.fit(Xtrain_sel, ytrain)
+
+# %%
+study = svc_search.study
+train_params = svc_search.best_params
+
+# %% [markdown]
+# # Entrenamiento de de modelo final
+
+# %%
+model = SVC(
+    C=train_params['C'],
+    kernel='linear',
+    probability=True,
+    class_weight='balanced',
+    random_state=seed,
+)
+
+# %%
+joblib.dump(learner, data_to + 'learner_svc_' + ventana + '.joblib')
+joblib.dump(study, data_to + 'study_svc_' + ventana + '.joblib')
+joblib.dump(model, data_to + 'model_svc_' + ventana + '.joblib')
